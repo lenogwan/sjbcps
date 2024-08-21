53365a6ba0974b8d90b5e3f4ca352db13423b5a7
lenogwan <31268499+lenogwan@users.noreply.github.com>
2024-08-21 14:12:36 +0800

    Update SJBCPS-Login-script.bat

--- SJBCPS-Login-script.bat
+++ SJBCPS-Login-script.bat
@@ -8,5 +8,4 @@ net use h: \\stjohn.edu.hk\fileshare\home\%id% /user:stjohn\%id% /persistent:yes
 net use q: \\stjohn.edu.hk\fileshare\student /user:stjohn\%id% /persistent:yes
 net use s: \\stjohn.edu.hk\fileshare\Share /user:stjohn\%id% /persistent:yes
 net use v: \\stjohn.edu.hk\fileshare\public /user:stjohn\%id% /persistent:yes
-rundll32 printui.dll PrintUIEntry /in /n\\sjserver\SAMS_RM_13_NONDOMAIN
-rundll32 printui.dll PrintUIEntry /in /n\\sjserver\SAMS_RM_14_NONDOMAIN
+
