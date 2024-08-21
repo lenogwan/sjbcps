20191b3cc347899ce7dc05d107d141f3b5004a5e
lenogwan <31268499+lenogwan@users.noreply.github.com>
2024-08-21 09:43:08 +0800

    Update SJBCPS-Login-script.bat

--- SJBCPS-Login-script.bat
+++ SJBCPS-Login-script.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET /p id=Enter Enter your SJBCPS user name:
+SET /p id=Please Enter your SJBCPS user name:
 NET USE H: /DELETE >nul 2>&1
 NET USE Q: /DELETE >nul 2>&1
 NET USE S: /DELETE >nul 2>&1
@@ -7,6 +7,6 @@ NET USE V: /DELETE >nul 2>&1
 net use h: \\stjohn.edu.hk\fileshare\home\%id% /user:stjohn\%id% /persistent:yes
 net use q: \\stjohn.edu.hk\fileshare\student /user:stjohn\%id% /persistent:yes
 net use s: \\stjohn.edu.hk\fileshare\Share /user:stjohn\%id% /persistent:yes
-net use v: \\stjohn.edu.hk\fileshare\public /persistent:yes
+net use v: \\stjohn.edu.hk\fileshare\public /user:stjohn\%id% /persistent:yes
 rundll32 printui.dll PrintUIEntry /in /n\\sjserver\SAMS_RM_13_NONDOMAIN
 rundll32 printui.dll PrintUIEntry /in /n\\sjserver\SAMS_RM_14_NONDOMAIN
