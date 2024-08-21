da70e401d684ff9887e83704c9f677f43e5536dd
lenogwan <31268499+lenogwan@users.noreply.github.com>
2024-08-21 08:21:33 +0800

    Add files via upload

---
+++ SJBCPS-Login-script.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SET /p id=Enter Enter your SJBCPS user name:
+NET USE H: /DELETE >nul 2>&1
+NET USE Q: /DELETE >nul 2>&1
+NET USE S: /DELETE >nul 2>&1
+NET USE V: /DELETE >nul 2>&1
+net use h: \\stjohn.edu.hk\fileshare\home\%id% /user:stjohn\%id% /persistent:yes
+net use q: \\stjohn.edu.hk\fileshare\student /user:stjohn\%id% /persistent:yes
+net use s: \\stjohn.edu.hk\fileshare\Share /user:stjohn\%id% /persistent:yes
+net use v: \\stjohn.edu.hk\fileshare\public /persistent:yes
